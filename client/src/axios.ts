48a58f7a358694d9b2504a50b60389c8d93f4e97
Iván Roldán Lusich <ivanrl2010@gmail.com>
2020-05-04 16:48:37 -0300

    set axios to new endpoint in production

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+console.log(process.env.REACT_API_ENDPOINT);
+console.log(process.env.NODE_ENV);
 export const API_ENDPOINT =
-  process.env.NODE_ENV === 'development' ? '/api' : 'http://localhost:5000/api';
-//process.env.REACT_APP_API_ENDPOINT ||
+  process.env.REACT_APP_API_ENDPOINT || 'http://localhost:5000';
 
 const headers = {
   'Content-Type': 'application/json',
